535f5a52e49d1bfb127f8c322725beca7c0745d1
Abdallah A. Shafy <91617960+Abdallah0101@users.noreply.github.com>
2025-01-04 10:11:42 -0300

    Update problem01
    Passing by const reference to avoid unnecessary copying

--- Problem01.cpp
+++ Problem01.cpp
@@ -1,5 +1,6 @@
 /*
- * Program to print your name on the screen.
+ * Printing a Name on the Screen
+ * This program prints a predefined name to the screen using a function.
  */
 
 #include <iostream>
@@ -8,13 +9,11 @@
 using namespace std;
 
 // Function to print the name
-void printName(string Name)
-{
-    cout << "\n Your Name is: " << Name << endl;
+void printName(const string& name) { // Passing by const reference to avoid unnecessary copying
+    cout << "\nYour Name is: " << name << endl;
 } 
 
-int main()
-{
+int main() {
     printName("Abdalla");  // Call the function with the name
     return 0;
 }
